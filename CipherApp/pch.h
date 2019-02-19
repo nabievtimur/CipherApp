6ba665d655953f9243cdba7fdc05f4da70dc2d7f
Nabiev Timur <eragon96@inbox.ru>
2019-02-19 15:20:02 +0300

    Create CMake file

--- CipherApp/pch.h
+++ CipherApp/pch.h
@@ -10,4 +10,15 @@
 #include "MathCore.h"
 #include "Utills.h"
 
+#if defined(__linux) || defined(__linux__) || defined(linux)
+# define LINUX
+
+#elif defined(__APPLE__)
+# define MACOS
+
+#elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32) || defined(_WIN64)
+# define WINDOWS
+
+#endif
+
 #endif //PCH_H
